f3f8839e8181f35066ef71b82f54c110442ecee1
Lewis <159625440+LewisSimatwa@users.noreply.github.com>
2024-04-03 23:52:31 +0300

    Update debbieinsert.php

--- Store_pages/debbieinsert.php
+++ Store_pages/debbieinsert.php
@@ -2,8 +2,7 @@
 include 'dbconnect.php';
 include 'dbfields.php';
 
-$sql='INSERT INTO `debbie`(`item_ID`, `b_ID`, `item_name`, `flavour`, `quantity`, `price`) VALUES ("[value-1]","[value-2]","[value-3]","[value-4]","[value-5]","[value-6]")';
-
+$sql = "INSERT INTO `debbie`(`item_ID`, `b_ID`, `item_name`, `flavour`, `quantity`, `price`) VALUES ('$item_ID', '$b_ID', '$item_name', '$flavour', '$quantity', '$price')";
 
 if (mysqli_query($conn, $sql)) {
   echo "A new record has been created successfully";
