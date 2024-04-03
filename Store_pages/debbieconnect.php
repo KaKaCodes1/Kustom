05e9109fa2292de3cae34aaf8aeb0f96b5a5507d
Lewis <159625440+LewisSimatwa@users.noreply.github.com>
2024-04-03 21:15:30 +0300

    Update the db name

--- Store_pages/debbieconnect.php
+++ Store_pages/debbieconnect.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "kustom";
+$db = "kustom2";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$db);
