8f92cd0f44efbb1d71ea27e32ff16a092f742b05
Sharon Kitavi <120562558+sharonkitavi@users.noreply.github.com>
2024-04-07 12:25:00 +0300

    Update update.php

--- Kustom_Customer_Profile/update.php
+++ Kustom_Customer_Profile/update.php
@@ -21,16 +21,20 @@ if ($conn->connect_error) {
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Retrieve form data
-    $customerId = $_POST['customer_id'];
-    $name = $_POST['name'];
+    $customerId = $_POST['c_ID'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
     $email = $_POST['email'];
-    $address = $_POST['address'];
+    $address = $_POST['p_address'];
     $phone = $_POST['phone'];
+    $pass = $_POST['password2']
 
     // Prepare SQL statement to update customer profile
-    $sql = "UPDATE customer_profile SET name=?, email=?,p_address=?, phone=? WHERE id=?";
+    $sql = "UPDATE customer_profile SET fname=$fname, lname=$lname, email=$email,p_address=$address, phone=$phone, password2=$pass WHERE c_ID=customerId";
 
-
+// Prepare and bind parameters
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssii", $fname, $lname, $email, $address, $phone, $pass, $customerId);
 
  // Execute the statement
 $stmt->execute();
