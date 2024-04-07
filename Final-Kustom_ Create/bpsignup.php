8827a679400404cdf80a16c702d0492c11d03235
Hamisi_Juma <hamisig123@gmail.com>
2024-04-07 16:26:55 +0300

    made changes on the names of the form inputs in the business sign up

--- Final-Kustom_ Create/bpsignup.php
+++ Final-Kustom_ Create/bpsignup.php
@@ -23,13 +23,14 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fname = mysqli_real_escape_string($conn, $_POST["fname"]);
     $lname = mysqli_real_escape_string($conn, $_POST["lname"]);
-    $b_name = mysqli_real_escape_string($conn, $_POST["b_name"]);
-    $p_email = mysqli_real_escape_string($conn, $_POST["p_email"]);
-    $b_email = mysqli_real_escape_string($conn, $_POST["b_email"]);
-    $b_description = mysqli_real_escape_string($conn, $_POST["b_description"]);
+    $p_email = mysqli_real_escape_string($conn, $_POST["emailInput"]);
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $b_name = mysqli_real_escape_string($conn, $_POST["bname"]);
+    $b_description = mysqli_real_escape_string($conn, $_POST["description"]);
+    $b_email = mysqli_real_escape_string($conn, $_POST["b_emailInput"]);
+
 }
-    $sql = "INSERT INTO business_profile (fname, lname, b_name, p_email, b_email, b_description password) VALUES ('$fname', '$lname', '$b_name', '$p_email', '$b_email', '$b_description' '$password')";
+    $sql = "INSERT INTO business_profile (fname, lname, b_name, p_email, b_email, b_description ,password) VALUES ('$fname', '$lname', '$b_name', '$p_email', '$b_email', '$b_description', '$password')";
     
 
 if ($conn->query($sql) === TRUE) {
